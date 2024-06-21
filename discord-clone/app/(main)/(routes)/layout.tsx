200b31eb331a5a5c944c097fb42441759cb898c2
mario-aj <mariojorge1997@mail.ru>
2024-06-21 17:45:06 +0100

    server layout structure

---
+++ discord-clone/app/(main)/(routes)/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+
+interface MainLayoutProps {
+  children: ReactNode
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <div className='h-full'>
+      <div className='hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0'></div>
+
+      <main className='md:pl-[72px] h-full'>{children}</main>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ discord-clone/app/(main)/(routes)/servers/[serveId]/page.tsx
@@ -0,0 +1,5 @@
+const ServerIdPage = () => {
+  return <div>server page id</div>
+}
+
+export default ServerIdPage
