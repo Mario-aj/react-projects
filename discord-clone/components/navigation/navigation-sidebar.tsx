dcb9c335bbe8e0f37a6cd1244f75771022519edf
mario-aj <mariojorge1997@mail.ru>
2024-06-22 16:29:30 +0100

    navigationItem component done

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -1,28 +1,45 @@
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 
-import { db } from '@/lib/db'
-import { currentProfile } from '@/lib/current-profile'
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { Separator } from "@/components/ui/separator";
 
-import { NavigationAction } from './navigation-action'
+import { NavigationAction } from "./navigation-action";
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
-  const profile = await currentProfile()
+  const profile = await currentProfile();
 
-  if (!profile) return redirect('/')
+  if (!profile) return redirect("/");
 
   const servers = await db.server.findMany({
     where: {
       members: {
         some: {
-          profileId: profile.id
-        }
-      }
-    }
-  })
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
   return (
-    <div className='pace-y-4 flex flex-col items-center h-full text-primary  w-full dark:bg-[#1E1F22] py-3'>
+    <div className="space-y-4 flex flex-col items-center h-full text-primary  w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
-  )
-}
+  );
+};
