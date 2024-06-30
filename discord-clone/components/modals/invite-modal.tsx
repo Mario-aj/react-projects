fabe8f4e1fb434e5f28ebc83cbad50b966f3d747
mario-aj <mariojorge1997@mail.ru>
2024-06-30 20:55:57 +0100

    create invite people modal

---
+++ discord-clone/components/modals/invite-modal.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogContent,
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+
+export const InviteModal = () => {
+  const origin = useOrigin();
+  const { isOpen, type, onClose, data } = useModal();
+
+  const [copied, setCopied] = useState(false);
+
+  const { server } = data;
+  const inviterUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(inviterUrl);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <Dialog open={isOpen && type === "invite"} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Invite friends
+          </DialogTitle>
+        </DialogHeader>
+
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+
+          <div className="flex items-cente mt-2 gap-x-2">
+            <Input
+              value={inviterUrl}
+              className="bg-zinc-300/50 border-0 focus-visible:ronge-0 text-black focus-visible:ring-offset-0"
+            />
+
+            <Button size="icon" onClick={handleCopy}>
+              {copied ? (
+                <Check className="w-4 h-4" />
+              ) : (
+                <Copy className="w-4 h-4" />
+              )}
+            </Button>
+          </div>
+
+          <Button
+            size="sm"
+            variant="link"
+            className="text-xs text-zinc-500 mt-4"
+          >
+            Generate a new link
+            <RefreshCw className="w-4 h-4 ml-2" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InviteModal;
