72221c4369844d136780c7d0813c7a84c272ad43
mario-aj <mariojorge1997@mail.ru>
2024-07-04 20:35:01 +0100

    leaver server featute had just been released right now xD :)

--- discord-clone/components/server/server-header.tsx
+++ discord-clone/components/server/server-header.tsx
@@ -92,7 +92,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
 
         {!isAdmin && (
-          <DropdownMenuItem className=" text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
+            className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Leave Server
             <LogOut className="h-4 ww-4 ml-auto" />
           </DropdownMenuItem>
