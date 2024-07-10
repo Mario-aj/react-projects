8208ae87a5bfa9558bca073660b25b00b50249e4
mario-aj <mariojorge1997@mail.ru>
2024-07-10 22:11:19 +0100

    feat(chat): config the useChatQuery hooks

--- discord-clone/components/chat/chat-messages.tsx
+++ discord-clone/components/chat/chat-messages.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { Member } from "@prisma/client";
+import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+
 import { ChatWelcome } from "./chat-welcome";
 
 interface ChatMessageProps {
@@ -26,6 +30,33 @@ export const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessageProps) => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useChatQuery({ queryKey: `chat:${chatId}`, apiUrl, paramKey, paramValue });
+
+  if (status === "pending") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+
+        <p className="text-sm text-zinc-500 dark:text-zinc-400 ">
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+
+        <p className="text-sm text-zinc-500 dark:text-zinc-400 ">
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
---
+++ discord-clone/hooks/use-chat-query.ts
@@ -0,0 +1,47 @@
+import qs from "query-string";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { useSocket } from "@/components/providers/socket-provider";
+
+interface UseChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+
+export const useChatQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: UseChatQueryProps) => {
+  const { isConnected } = useSocket();
+
+  const fetchMessages = async ({ pageParam = undefined }) => {
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+
+    const res = await fetch(url);
+
+    return res.json();
+  };
+
+  const inifiteQuery = useInfiniteQuery({
+    initialPageParam: undefined,
+    queryKey: [queryKey],
+    queryFn: fetchMessages,
+    getNextPageParam: (lastPage) => lastPage?.nextCursor,
+    refetchInterval: isConnected ? false : 1000,
+  });
+
+  return inifiteQuery;
+};
