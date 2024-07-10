c83f6f158f7a29f9317d87f276d151c83001a488
mario-aj <mariojorge1997@mail.ru>
2024-07-10 14:33:51 +0100

    feat(chat): send file (image and pdf)

--- discord-clone/components/file-upload.tsx
+++ discord-clone/components/file-upload.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 
 interface FileUploadProps {
   endpoint: "messageFile" | "serverImage";
@@ -19,7 +19,32 @@ const FileUpload = ({ endpoint, onChange, value }: FileUploadProps) => {
 
         <button
           onClick={() => onChange("")}
-          className="bg-rose-500 text-white- p-1 rounded-full absolute right-0 shadow-sm"
+          className="bg-rose-500 text-white- p-1 top-0 rounded-full absolute right-0 shadow-sm"
+          type="button"
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white- p-1 rounded-full absolute -right-2 -top-3 shadow-sm"
           type="button"
         >
           <X className="w-4 h-4" />
---
+++ discord-clone/components/modals/message-file-modal.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import FileUpload from "@/components/file-upload";
+import { useRouter } from "next/navigation";
+import { Server } from "@prisma/client";
+import { useModal } from "@/hooks/use-modal-store";
+import queryString from "query-string";
+
+const formSchema = z.object({
+  fileUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
+});
+export const MessageFileModal = () => {
+  const router = useRouter();
+  const { isOpen, onClose, type, data } = useModal();
+
+  const { query, apiUrl } = data;
+
+  const form = useForm({
+    defaultValues: {
+      fileUrl: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = queryString.stringifyUrl({
+        url: apiUrl ?? "",
+        query,
+      });
+
+      await axios.post<Server>(url, {
+        ...values,
+        content: values.fileUrl,
+      });
+
+      form.reset();
+
+      router.refresh();
+      handleClose();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Dialog open={isOpen && type === "messageFile"} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Add an attachment
+          </DialogTitle>
+
+          <DialogDescription className="text-center text-zinc-500">
+            Send a file as a message
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="fileUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="messageFile"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+                Send
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
