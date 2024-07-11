fe0b0d66bfb6297c4367d04dd9bc4cb80485dd69
mario-aj <mariojorge1997@mail.ru>
2024-07-11 22:41:46 +0100

    feat(chat): delete message from channel

--- discord-clone/components/chat/chat-item.tsx
+++ discord-clone/components/chat/chat-item.tsx
@@ -16,6 +16,7 @@ import { ActionTooltip } from "@/components/action-tooltip";
 import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps {
   id: string;
@@ -54,8 +55,9 @@ export const ChatItem = ({
   socketQuery,
   socketUrl,
 }: ChatItemProps) => {
+  const { onOpen } = useModal();
+
   const [isEditing, setIsEditing] = useState(false);
-  const [isDeleting, setIsDeleting] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -228,7 +230,15 @@ export const ChatItem = ({
           )}
 
           <ActionTooltip label="Delete">
-            <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+            <Trash
+              onClick={() =>
+                onOpen("deleteMessage", {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+              className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
