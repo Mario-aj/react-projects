e823d981af8daa3d427e83824a6df08afb0ed912
mario-aj <mariojorge1997@mail.ru>
2024-07-14 12:47:54 +0100

    feat: config clerk to work with dark mode

--- threads/components/shared/top-bar.tsx
+++ threads/components/shared/top-bar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 export function Topbar() {
   return (
@@ -29,6 +30,7 @@ export function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
