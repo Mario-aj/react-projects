9299648d89e191e56af4c8f8b1a5f806e43840db
mario-aj <mariojorge1997@mail.ru>
2024-07-22 21:34:10 +0100

    feat(community): create community functionality

---
+++ threads/lib/models/community.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { InferSchemaType } from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema);
+
+export type CommunityType = InferSchemaType<typeof communitySchema>;
+
+export default Community;
