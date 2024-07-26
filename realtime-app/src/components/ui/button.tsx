583016903f46e1c426eecfc77596fc0ea2a42d75
mario-aj <mariojorge1997@mail.ru>
2024-07-26 21:33:38 +0100

    feat: create button component, add deps

---
+++ realtime-app/src/components/ui/button.tsx
@@ -0,0 +1,51 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
+import { ButtonHTMLAttributes } from "react";
+
+const buttonVariants = cva(
+  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:rinf-offset-2 disabled:opacity-50 disabled:point-events-none",
+  {
+    variants: {
+      varint: {
+        default: "bg-slate-900 text0white hover:bg-slate-800",
+        ghost: "",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2",
+        lg: "h-11 px-8",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+      varint: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+export const Button = ({
+  className,
+  children,
+  isLoading,
+  varint,
+  size,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={cn(buttonVariants({ varint, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-ping" /> : null}
+      {children}
+    </button>
+  );
+};
