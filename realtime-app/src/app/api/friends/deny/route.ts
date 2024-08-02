98020f5e5953ec3341b8f29bde104f81042b86cd
mario-aj <mariojorge1997@mail.ru>
2024-08-02 17:56:32 +0100

    feat: deny friend request implementation

---
+++ realtime-app/src/app/api/friends/deny/route.ts
@@ -0,0 +1,30 @@
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import * as z from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) return new NextResponse("Unauthorized", { status: 401 });
+
+    const { id: idToDeny } = z.object({ id: z.string() }).parse(body);
+
+    await db.srem(
+      `user:${session.user.id}:incomming_friend_requests`,
+      idToDeny
+    );
+
+    return NextResponse.json({});
+  } catch (error: any) {
+    if (error instanceof z.ZodError) {
+      return new NextResponse("Invalide request payload", { status: 422 });
+    }
+
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
