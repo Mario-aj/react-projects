5fc4a8c0b55a6dd1ef70905a8f503017cbf9027b
mario-aj <mariojorge1997@mail.ru>
2024-08-15 11:02:48 +0100

    feat: generate supabase types

---
+++ slacky/src/types/app.d.ts
@@ -0,0 +1,12 @@
+export interface User {
+  avatar_url: string
+  channels: string[] | null
+  created_at: string | null
+  email: string
+  id: string
+  is_away: boolean
+  name: string | null
+  phone: string | null
+  type: string | null
+  workplaces: string[] | null
+} 
